57ac659a20d0085e5a233a492dba261fd3390f34
Craig Sparks <sparks.craig@gmail.com>
2013-01-16 22:18:28 -0500

    Finalizing updates to wrapper

--- classes/sitemap/core.php
+++ classes/sitemap/core.php
@@ -12,26 +12,42 @@ class Sitemap_Core {
 		$this->_config = Kohana::$config->load('sitemap.'.$config);
 
 		//Initialize new generator file
-		$this->_generator = new cXmlSitemapGeneratorWrite($this->_config['filename'], $this->_config['compress'], $this->_config['path']);
+		$this->_generator = new cXmlSitemapGeneratorWrite($this->_config['filename'], $this->_config['compression'], $this->_config['path']);
 	}
 
 	public function delete_current()
 	{
-
+		$this->_generator->deleteCurrent();
 	}
 
 	public function open()
 	{
-
+		$this->_generator->open();
 	}
 
 	public function save()
 	{
-
+		$this->_generator->save();
 	}
 
-	public function update_index()
+	public function add_url($url, $last_modified = NULL, $change_frequency = NULL, $priority = NULL)
 	{
+		$this->_generator->addUrl($url, $last_modified, $change_frequency, $priority);
+	}
 
+	public function update_index($file)
+	{
+		$this->_generator->updateSitemapIndex($file);
 	}
 }
+
+//Include the LargeXMLSitemap init file(will include all needed files for LargeXMLSitemap)
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/abstract.xmlSitemapBase');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/abstract.xmlSitemapGeneratorBase');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemapConfig');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemapGenerator');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemapGeneratorWrite');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemapIndex');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemapMaximumError');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemap');
+require Kohana::find_file('vendor/LargeXMLSitemap', 'src/classes/class.xmlSitemapWrite');
